cb478399f886b6ba0318ef6ebaba8cdc756da96c
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2025-04-18 16:51:45 +0900

    feat: [이재림] 덱 삭제 버튼 배치 [ETWGL-269]

--- src/deck_delete_button_position/repository/DeckDeleteButtonPositionRepositoryImpl.ts
+++ src/deck_delete_button_position/repository/DeckDeleteButtonPositionRepositoryImpl.ts
@@ -1,39 +1,39 @@
 import {Vector2d} from "../../common/math/Vector2d";
-import {DeckEditButtonPosition} from "../entity/DeckEditButtonPosition";
-import {DeckEditButtonPositionRepository} from "./DeckEditButtonPositionRepository";
+import {DeckDeleteButtonPosition} from "../entity/DeckDeleteButtonPosition";
+import {DeckDeleteButtonPositionRepository} from "./DeckDeleteButtonPositionRepository";
 
-export class DeckEditButtonPositionRepositoryImpl implements DeckEditButtonPositionRepository {
-    private static instance: DeckEditButtonPositionRepositoryImpl;
-    private positionMap: Map<number, { deckId: number, position: DeckEditButtonPosition }> = new Map(); // position unique id: {deck id: mesh}
+export class DeckDeleteButtonPositionRepositoryImpl implements DeckDeleteButtonPositionRepository {
+    private static instance: DeckDeleteButtonPositionRepositoryImpl;
+    private positionMap: Map<number, { deckId: number, position: DeckDeleteButtonPosition }> = new Map(); // position unique id: {deck id: mesh}
 
-    private positionX = - 0.321;
+    private positionX = - 0.277;
     private initialY = 0.135;
     private incrementY = - 0.09;
     private positionIndex = 0;
 
     private constructor() {}
 
-    public static getInstance(): DeckEditButtonPositionRepositoryImpl {
-        if (!DeckEditButtonPositionRepositoryImpl.instance) {
-            DeckEditButtonPositionRepositoryImpl.instance = new DeckEditButtonPositionRepositoryImpl();
+    public static getInstance(): DeckDeleteButtonPositionRepositoryImpl {
+        if (!DeckDeleteButtonPositionRepositoryImpl.instance) {
+            DeckDeleteButtonPositionRepositoryImpl.instance = new DeckDeleteButtonPositionRepositoryImpl();
         }
-        return DeckEditButtonPositionRepositoryImpl.instance;
+        return DeckDeleteButtonPositionRepositoryImpl.instance;
     }
 
-    public addDeckEditButtonPosition(deckId: number): DeckEditButtonPosition {
+    public addDeckDeleteButtonPosition(deckId: number): DeckDeleteButtonPosition {
         if (this.containsDeckIdInMap(deckId) == false) {
             this.positionIndex++;
         }
         const positionX = this.positionX;
         const positionY = this.initialY + (this.positionIndex - 1) * this.incrementY;
 
-        const newPosition = new DeckEditButtonPosition(positionX, positionY);
+        const newPosition = new DeckDeleteButtonPosition(positionX, positionY);
         this.positionMap.set(newPosition.id, { deckId, position: newPosition });
 
         return newPosition;
     }
 
-    public findPositionByPositionId(positionId: number): DeckEditButtonPosition | null {
+    public findPositionByPositionId(positionId: number): DeckDeleteButtonPosition | null {
         const position = this.positionMap.get(positionId);
         if (position) {
             return position.position;
@@ -42,7 +42,7 @@ export class DeckEditButtonPositionRepositoryImpl implements DeckEditButtonPosit
         }
     }
 
-    public findPositionByDeckId(deckId: number): DeckEditButtonPosition | null {
+    public findPositionByDeckId(deckId: number): DeckDeleteButtonPosition | null {
         for (const { deckId: storedDeckId, position } of this.positionMap.values()) {
             if (storedDeckId === deckId) {
                 return position;
@@ -51,7 +51,7 @@ export class DeckEditButtonPositionRepositoryImpl implements DeckEditButtonPosit
         return null;
     }
 
-    public findAllPosition(): DeckEditButtonPosition[] {
+    public findAllPosition(): DeckDeleteButtonPosition[] {
         return Array.from(this.positionMap.values()).map(({ position }) => position);
     }
 
@@ -72,7 +72,7 @@ export class DeckEditButtonPositionRepositoryImpl implements DeckEditButtonPosit
         this.positionMap.delete(positionId);
 
         let newPositionIndex = 0;
-        const newPositionMap = new Map<number, { deckId: number, position: DeckEditButtonPosition }>();
+        const newPositionMap = new Map<number, { deckId: number, position: DeckDeleteButtonPosition }>();
 
         for (const [key, { deckId, position }] of this.positionMap.entries()) {
             const newPositionY = this.initialY + (newPositionIndex * this.incrementY);
