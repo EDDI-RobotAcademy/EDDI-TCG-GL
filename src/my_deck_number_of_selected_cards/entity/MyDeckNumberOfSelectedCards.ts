569fbf21f60ad311e5665acf4258ca623e97624a
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2025-08-20 00:59:11 +0900

    feat: [이재림] 덱 편집 중 다른 덱 버튼 클릭 시 선택 카드 개수 상태 복원 [ETWGL-357]

--- src/my_deck_number_of_selected_cards/entity/MyDeckNumberOfSelectedCards.ts
+++ src/my_deck_number_of_selected_cards/entity/MyDeckNumberOfSelectedCards.ts
@@ -4,13 +4,11 @@ import {IdGenerator} from "../../common/id_generator/IdGenerator";
 
 export class MyDeckNumberOfSelectedCards {
     id: number;
-    isVisible: boolean;
     mesh: THREE.Mesh;
     position: Vector2d;
 
     constructor(mesh: THREE.Mesh, position: Vector2d) {
         this.id = IdGenerator.generateId("MyDeckNumberOfSelectedCards");
-        this.isVisible = true;
         this.mesh = mesh;
         this.position = position;
         this.mesh.visible = true;
@@ -21,11 +19,10 @@ export class MyDeckNumberOfSelectedCards {
     }
 
     public getVisibility(): boolean {
-        return this.isVisible;
+        return this.mesh.visible;
     }
 
     public setVisibility(state: boolean): void {
-        this.isVisible = state;
         this.mesh.visible = state;
     }
 
