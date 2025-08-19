abb991d3012af85c006effcf1a8c81b445800553
silenc3502 <gcccompil3r@gmail.com>
2025-08-19 22:56:46 +0900

    chore: remove useless code

--- src/action_type/repository/ActionTypeRepository.ts
+++
@@ -1,7 +0,0 @@
-import {ActionType} from "../entity/ActionType";
-
-export interface ActionTypeRepository {
-    getActionType(): ActionType;
-    setActionType(type: ActionType): void;
-    reset(): void;
-}
