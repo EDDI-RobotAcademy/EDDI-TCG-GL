aef847b784d86c467819d52dca3a3f5debf69747
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2025-09-11 23:38:15 +0900

    feat: [이재림] 검색 취소용 버튼 클릭 시 모든 덱 카드 원래 위치로 재배치 [ETWGL-391]

--- src/deck_card_search_cancel_button_click_detect/service/DeckCardSearchCancelButtonClickDetectServiceImpl.ts
+++ src/deck_card_search_cancel_button_click_detect/service/DeckCardSearchCancelButtonClickDetectServiceImpl.ts
@@ -5,20 +5,33 @@ import {DeckCardSearchCancelButtonClickDetectRepositoryImpl} from "../repository
 
 import {MyDeckCardSearchCancelButton} from "../../my_deck_card_search_cancel_button/entity/MyDeckCardSearchCancelButton";
 import {MyDeckCardSearchCancelButtonRepositoryImpl} from "../../my_deck_card_search_cancel_button/repository/MyDeckCardSearchCancelButtonRepositoryImpl";
+import {MyDeckButtonClickDetectRepositoryImpl} from "../../deck_button_click_detect/repository/MyDeckButtonClickDetectRepositoryImpl";
+import {MyDeckCardRepositoryImpl} from "../../my_deck_card/repository/MyDeckCardRepositoryImpl";
+import {MyDeckCardPositionRepositoryImpl} from "../../my_deck_card_position/repository/MyDeckCardPositionRepositoryImpl";
 
 import {CameraRepository} from "../../camera/repository/CameraRepository";
 import {CameraRepositoryImpl} from "../../camera/repository/CameraRepositoryImpl";
 
+import {MyDeckElementAdjuster} from "../../my_deck_element_adjuster/MyDeckElementAdjuster";
+
 export class DeckCardSearchCancelButtonClickDetectServiceImpl implements DeckCardSearchCancelButtonClickDetectService {
     private static instance: DeckCardSearchCancelButtonClickDetectServiceImpl | null = null;
     private cameraRepository: CameraRepository;
+    private myDeckElementAdjuster: MyDeckElementAdjuster;
     private deckCardSearchCancelButtonClickDetectRepository: DeckCardSearchCancelButtonClickDetectRepositoryImpl;
     private myDeckCardSearchCancelButtonRepository: MyDeckCardSearchCancelButtonRepositoryImpl;
+    private myDeckButtonClickDetectRepository: MyDeckButtonClickDetectRepositoryImpl;
+    private myDeckCardRepository: MyDeckCardRepositoryImpl;
+    private myDeckCardPositionRepository: MyDeckCardPositionRepositoryImpl;
 
     private constructor(private camera: THREE.Camera, private scene: THREE.Scene) {
         this.cameraRepository = CameraRepositoryImpl.getInstance();
+        this.myDeckElementAdjuster = MyDeckElementAdjuster.getInstance();
         this.deckCardSearchCancelButtonClickDetectRepository = DeckCardSearchCancelButtonClickDetectRepositoryImpl.getInstance();
         this.myDeckCardSearchCancelButtonRepository = MyDeckCardSearchCancelButtonRepositoryImpl.getInstance();
+        this.myDeckButtonClickDetectRepository = MyDeckButtonClickDetectRepositoryImpl.getInstance();
+        this.myDeckCardRepository = MyDeckCardRepositoryImpl.getInstance(scene);
+        this.myDeckCardPositionRepository = MyDeckCardPositionRepositoryImpl.getInstance();
     }
 
     static getInstance(camera: THREE.Camera, scene: THREE.Scene): DeckCardSearchCancelButtonClickDetectServiceImpl {
@@ -38,15 +51,20 @@ export class DeckCardSearchCancelButtonClickDetectServiceImpl implements DeckCar
 
     public async handleClick(clickPoint: { x: number; y: number }): Promise<MyDeckCardSearchCancelButton | null> {
         const { x, y } = clickPoint;
+        const currentClickedDeckId = this.myDeckButtonClickDetectRepository.getCurrentClickDeckButtonId();
+        if (currentClickedDeckId == null) return null;
+
         const button = this.getSearchCancelButton();
         if (button !== null) {
             const clickedButton = this.deckCardSearchCancelButtonClickDetectRepository.isButtonClicked(
                 { x, y },
                 button,
-                this.camera);
+                this.camera
+            );
 
             if (clickedButton) {
                 console.log(`[DEBUG] Clicked Search Cancel Button`);
+                this.restoreAllCardPositions(currentClickedDeckId);
 
                 return clickedButton;
             }
@@ -66,6 +84,29 @@ export class DeckCardSearchCancelButtonClickDetectServiceImpl implements DeckCar
 
     private getSearchCancelButton(): MyDeckCardSearchCancelButton | null {
         return this.myDeckCardSearchCancelButtonRepository.findButton();
+    }
+
+    private restoreAllCardPositions(deckId: number): void {
+        const cardUniqueIdList = this.myDeckCardRepository.findCardUniqueIdListByDeckId(deckId);
+        for (const cardUniqueId of cardUniqueIdList) {
+            const cardId = this.myDeckCardRepository.findCardIdByCardUniqueId(cardUniqueId);
+            if (cardId == null) return;
+
+            const card = this.myDeckCardRepository.findCardByDeckIdAndCardId(deckId, cardId);
+            if (card == null) return;
+            const cardMesh = card.getMesh();
+
+            const cardPosition = this.myDeckCardPositionRepository.findPositionByDeckIdAndCardId(deckId, cardId);
+            if (cardPosition == null) return;
+
+            const widthPercent = 0.096;
+            const heightPercent = (1540 / 952);
+            const positionX = cardPosition.getX();
+            const positionY = cardPosition.getY();
+
+            this.myDeckElementAdjuster.adjustElementPosition(cardMesh, widthPercent, heightPercent, positionX, positionY);
+            card.setVisibility(true);
+        }
     }
 
 }
