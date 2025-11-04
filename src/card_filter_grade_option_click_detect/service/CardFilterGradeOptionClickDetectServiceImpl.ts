4ba4bde3063ec50a28054d19a6869eb8ca22dbca
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2025-11-04 15:06:06 +0900

    feat: [이재림] 등급 카테고리의 옵션 버튼 클릭 시 활성화된 옵션 버튼으로 교체 [ETWGL-433]

--- src/card_filter_grade_option_click_detect/service/CardFilterGradeOptionClickDetectServiceImpl.ts
+++ src/card_filter_grade_option_click_detect/service/CardFilterGradeOptionClickDetectServiceImpl.ts
@@ -10,17 +10,20 @@ import {CardFilterGradeOptionInactiveRepositoryImpl} from "../../card_filter_gra
 
 import {CameraRepository} from "../../camera/repository/CameraRepository";
 import {CameraRepositoryImpl} from "../../camera/repository/CameraRepositoryImpl";
+import {CardFilterGradeOptionActiveRepositoryImpl} from "../../card_filter_grade_option_active/repository/CardFilterGradeOptionActiveRepositoryImpl";
 
 export class CardFilterGradeOptionClickDetectServiceImpl implements CardFilterGradeOptionClickDetectService {
     private static instance: CardFilterGradeOptionClickDetectServiceImpl | null = null;
     private cameraRepository: CameraRepository;
     private cardFilterGradeOptionClickDetectRepository: CardFilterGradeOptionClickDetectRepositoryImpl;
     private cardFilterGradeOptionInactiveRepository: CardFilterGradeOptionInactiveRepositoryImpl;
+    private cardFilterGradeOptionActiveRepository: CardFilterGradeOptionActiveRepositoryImpl;
 
     private constructor(private camera: THREE.Camera, private scene: THREE.Scene) {
         this.cameraRepository = CameraRepositoryImpl.getInstance();
         this.cardFilterGradeOptionClickDetectRepository = CardFilterGradeOptionClickDetectRepositoryImpl.getInstance();
         this.cardFilterGradeOptionInactiveRepository = CardFilterGradeOptionInactiveRepositoryImpl.getInstance(scene);
+        this.cardFilterGradeOptionActiveRepository = CardFilterGradeOptionActiveRepositoryImpl.getInstance(scene);
     }
 
     static getInstance(camera: THREE.Camera, scene: THREE.Scene): CardFilterGradeOptionClickDetectServiceImpl {
@@ -42,6 +45,7 @@ export class CardFilterGradeOptionClickDetectServiceImpl implements CardFilterGr
         if (clickedOption) {
             const currentClickedOptionType = clickedOption.type;
             console.log(`[DEBUG] Click Card Filter Grade Option Type: ${currentClickedOptionType}`);
+            this.handleFilterGradeOptionToggle(currentClickedOptionType);
 
             return clickedOption;
         }
@@ -50,7 +54,7 @@ export class CardFilterGradeOptionClickDetectServiceImpl implements CardFilterGr
     }
 
     public async onMouseDown(event: MouseEvent): Promise<CardFilterGradeOptionInactive | null> {
-        if (!this.isAllOptionClickEnabled()) return null;
+        if (!this.isAllGradeOptionClickEnabled()) return null;
 
         if (event.button === 0) {
             const clickPoint = { x: event.clientX, y: event.clientY };
@@ -60,24 +64,53 @@ export class CardFilterGradeOptionClickDetectServiceImpl implements CardFilterGr
         return null;
     }
 
+    private handleFilterGradeOptionToggle(optionType: CardGrade): void {
+        const prevClickedOptionState = this.getCardFilterGradeOptionClickState(optionType);
+        if (prevClickedOptionState == true) {
+            // 이전에 클릭했을 때
+            this.updateGradeOptionState(optionType, true);
+        } else {
+            // 이전에 클릭하지 않았을 때
+            this.updateGradeOptionState(optionType, false);
+        }
+    }
+
+    private updateGradeOptionState(type: CardGrade, isActive: boolean): void {
+        this.saveCardFilterGradeOptionClickState(type, !isActive);
+        this.updateGradeOptionVisibility(type, isActive);
+    }
+
+    private updateGradeOptionVisibility(type: CardGrade, isActive: boolean): void {
+        this.setCardFilterGradeOptionInactiveVisibility(type, isActive);
+        this.setCardFilterGradeOptionActiveVisibility(type, !isActive);
+    }
+
     private getAllCardFilterGradeOptionInactives(): CardFilterGradeOptionInactive[] {
         return this.cardFilterGradeOptionInactiveRepository.findAllGradeOptions();
     }
 
-    private setAllOptionClickEnabled(isEnabled: boolean): void {
+    private setAllGradeOptionClickEnabled(isEnabled: boolean): void {
         this.cardFilterGradeOptionClickDetectRepository.setAllOptionClickEnabled(isEnabled);
     }
 
-    private isAllOptionClickEnabled(): boolean {
+    private isAllGradeOptionClickEnabled(): boolean {
         return this.cardFilterGradeOptionClickDetectRepository.isAllOptionClickEnabled();
     }
 
-    private saveCardFilterRaceOptionClickState(type: CardGrade, state: boolean): void {
+    private saveCardFilterGradeOptionClickState(type: CardGrade, state: boolean): void {
         this.cardFilterGradeOptionClickDetectRepository.saveOptionClickState(type, state);
     }
 
-    private getCardFilterRaceOptionClickState(type: CardGrade): boolean | undefined {
+    private getCardFilterGradeOptionClickState(type: CardGrade): boolean | undefined {
         return this.cardFilterGradeOptionClickDetectRepository.findOptionClickState(type);
+    }
+
+    private setCardFilterGradeOptionInactiveVisibility(type: CardGrade, isVisible: boolean): void {
+        this.cardFilterGradeOptionInactiveRepository.findGradeOptionByType(type)?.setVisibility(isVisible);
+    }
+
+    private setCardFilterGradeOptionActiveVisibility(type: CardGrade, isVisible: boolean): void {
+        this.cardFilterGradeOptionActiveRepository.findGradeOptionByType(type)?.setVisibility(isVisible);
     }
 
 }
